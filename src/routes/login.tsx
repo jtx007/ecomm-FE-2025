f650281af3119ad5814efac07fa3fbe97ec02a5e
jtx007 <jamesjacobthomas7@gmail.com>
2025-05-06 12:53:51 -0400

    add tabs for sign up and login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,9 +1,79 @@
 import { createFileRoute } from '@tanstack/react-router';
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
 export const Route = createFileRoute('/login')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/login"!</div>;
+  return (
+    <>
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="sign-up">Sign Up</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>Login with your username and password</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="username">Username</Label>
+                <Input autoComplete="true" id="username" placeholder="@jtx007" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">password</Label>
+                <Input id="password" type="password" placeholder="********" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Login</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="sign-up">
+          <Card>
+            <CardHeader>
+              <CardTitle>Sign Up</CardTitle>
+              <CardDescription>Sign Up for a new shopper's account</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" placeholder="James Thomas" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" placeholder="******" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password-confirm">Confirm Password</Label>
+                <Input id="password-confirm" type="password" placeholder="******" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="jamestdev7@gmail.com" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Sign Up</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </>
+  );
 }
